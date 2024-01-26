ad8178b8ff33160f5ed1ad4b11ec279804011d85
Viviane <dessaintviviane@gmail.com>
2024-01-26 20:33:01 +0100

    Update fetchError style

--- Playback/components/Error/fetchError.style.js
+++ Playback/components/Error/fetchError.style.js
@@ -7,22 +7,29 @@ export const styles = StyleSheet.create({
 		position: 'absolute',
 		bottom: 0,
 		flexDirection: 'row',
-		backgroundColor: COLORS.tertiary,
+		backgroundColor: COLORS.secondary,
 		padding: SIZES.xSmall,
 		borderRadius: SIZES.xSmall,
-		width: '100%',
+		width: '85%',
+		alignSelf: 'center',
+		borderWidth: 1,
+		borderColor: COLORS.primary,
+		alignItems: 'center',
 	},
 	fetchErrorTextContainer: {
-
+		flex: 1,
 	},
 	fetchErrorText: {
 		color: COLORS.warning,
+		fontFamily: FONTS.medium,
 	},
 	retryFetchButton: {
-		backgroundColor: COLORS.primary,
+		backgroundColor: COLORS.tertiary,
 	},
 	fetchErrorRetryText: {
 		color: COLORS.warning,
+		fontFamily: FONTS.bold,
+		textDecorationLine: 'underline',
 }});
 
 export default styles;
