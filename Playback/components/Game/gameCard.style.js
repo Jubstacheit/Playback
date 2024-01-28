1fc92233dfc35042aefe927f1abab161e4c8a5ac
Viviane <dessaintviviane@gmail.com>
2024-01-28 23:34:22 +0100

    Remove commented out code in gameCard.style.js

--- Playback/components/Game/gameCard.style.js
+++ Playback/components/Game/gameCard.style.js
@@ -59,29 +59,6 @@ const styles = StyleSheet.create({
 			}
 		})
 	}
-	/*container: {
-    flex: 1,
-    margin: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
-  },
-  gameImage: {
-    width: '100%',
-    height: 200,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-  },
-  gameTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333',
-    padding: 10,
-  }, */
 });
 
 export default styles;
