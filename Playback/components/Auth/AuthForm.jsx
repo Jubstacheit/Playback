a969c6cdabc794c705120b4a93cac8c7fed6f36b
Viviane <dessaintviviane@gmail.com>
2024-02-17 00:37:36 +0100

    Refactor AuthForm component and add Formik integration

--- Playback/components/Auth/AuthForm.jsx
+++ Playback/components/Auth/AuthForm.jsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import { Formik } from 'formik'
+import { Button, TextInput } from 'react-native-paper'
+import { COLORS } from '../../constants'
+
+import styles from './authForm.style'
 
 const AuthForm = () => {
 	return (
-		<View>
-			<Text>AuthForm</Text>
+		<View style={styles.container}>
+			<Formik
+				initialValues={{ email: '' }}
+				onSubmit={values => console.log(values)}
+			>
+			{({ handleChange, handleBlur, handleSubmit, values }) => (
+				<View style={styles.form}>
+				<TextInput
+					onChangeText={handleChange('email')}
+					onBlur={handleBlur('email')}
+					value={values.email}
+				/>
+				<Button 
+					mode='contained'
+					buttonColor={COLORS.secondary}
+					dark={true}
+					onPress={handleSubmit}
+				>
+					Submit
+				</Button>
+				</View>
+			)}
+			</Formik>
 		</View>
 	)
 }
-	
+		
 export default AuthForm
