34629c611f28c63475db245c55185296b358d86c
Viviane <dessaintviviane@gmail.com>
2024-05-01 22:21:18 +0200

    Revert "Refactor Express backend API by updating database configuration and index.js"
    This reverts commit 4660d840e4569446dc7fa2b19a52c7af7f26e649.

--- express-backend/api/index.js
+++ express-backend/api/index.js
@@ -1,28 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./config/database');
-require('dotenv').config();
 
-app.get('/', function(req, res) {
-	res.send('hello world');
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Import routes
-/*const userRoutes = require('../routes/userRoutes');
-// const gameRoutes = require('./routes/gameRoutes');
+app.listen(4000, () => console.log("Server ready on port 4000."));
 
-// Connect to the database
-db.authenticate()
-	.then(() => console.log('Database connected'))
-	.catch((err) => console.error('Error connecting to database:', err));
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Routes
-app.use('/user', userRoutes);
-*/
-const PORT = process.env.MYSQL_PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
