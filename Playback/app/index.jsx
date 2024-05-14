87739002a3908481844dc35546dfecbc084bd90f
Viviane <dessaintviviane@gmail.com>
2024-05-14 03:20:15 +0200

    chore: Update npm dependencies and remove unused code

--- Playback/app/index.jsx
+++ Playback/app/index.jsx
@@ -1,6 +1,5 @@
 import { useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getGamesHome } from "../hooks/RAWGService";
 
@@ -9,31 +8,36 @@ import styles from "../styles/home";
 import Title from "../components/Title/Title.jsx";
 import GameList from "../components/Game/GameList.jsx";
 
+import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config"
+
 const Home = () => {
   const router = useRouter();
   const { games, isLoading, error, refetch, retryFetch } = getGamesHome();
 
   return (
-    <SafeAreaView style={styles.container} >
-      <StatusBar style="dark"/>
-      {/* Title */}
-      <Title />
-      <Text style={styles.subtitle}>
-        Explore, list, backlog
-      </Text>
+    <TamaguiProvider config={tamaguiConfig}>
+      <SafeAreaView style={styles.container} >
+        <StatusBar style="dark"/>
+        {/* Title */}
+        <Title />
+        <Text style={styles.subtitle}>
+          Explore, list, backlog
+        </Text>
 
-      <Text style={styles.recentGames}>Recent games</Text>
-      
+        <Text style={styles.recentGames}>Recent games</Text>
+        
 
-      {/* Game list with home games*/}
-      <GameList 
-        games={games}
-        retryFetch={retryFetch}
-        isLoading={isLoading}
-        error={error}
-        refetch={refetch}
-      />
-    </SafeAreaView>
+        {/* Game list with home games*/}
+        <GameList 
+          games={games}
+          retryFetch={retryFetch}
+          isLoading={isLoading}
+          error={error}
+          refetch={refetch}
+        />
+      </SafeAreaView>
+    </TamaguiProvider>
   );
 }
 
