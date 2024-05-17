b09e12edc6313c97f1d5c241f0900da16d5f307e
Viviane <dessaintviviane@gmail.com>
2024-05-18 01:18:06 +0200

    chore: Update AuthForm component to include Formik for form handling

--- Playback/components/Auth/AuthForm.jsx
+++ Playback/components/Auth/AuthForm.jsx
@@ -1,50 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import { ActivityIndicator } from 'react-native'
-import { Form, H4, Button } from 'tamagui';
-import { COLORS } from '../../constants';
+import { Form, H2, H4, Button, Input, View } from 'tamagui';
+import { COLORS, SIZES } from '../../constants';
+import { Formik } from 'formik';
 
 const AuthForm = () => {
 
 	const [status, setStatus] = useState('off');
 
-	useEffect(() => {
-		if (status === 'submitting') {
-			const timer = setTimeout(() => setStatus('off'), 2000)
-			return () => {
-				clearTimeout(timer)
-			}
-		}
-	}, [status])
-
 
 	return (
-		<Form
-			flex={1}
-			alignItems='center'
-			justifyContent='center'
-			onSubmit={() => setStatus('submitting')}
-			borderWidth={1}
-			borderRadius='$4'
-			backgroundColor={COLORS.lightWhite}
-			padding='$8'
-			gap='$2'
-			alignSelf='center'
-			width={300}
+		<Formik
+			initialValues={{ username: '', email: '', password: '', confirmPassword: '' }}
+			onSubmit={(values) => {
+				setStatus('loading')
+				console.log(values)
+				setTimeout(() => {
+					setStatus('off')
+				}, 2000)
+			}}
 		>
-			<H4>AuthForm</H4>
-			<Form.Trigger asChild disabled={status !== 'off'}>
-				<Button 
-					disabledStyle={{opacity: 0.7}} 
-					icon={status === 'submitting' ? () => <ActivityIndicator size="small" color={COLORS.secondary} /> : undefined}
-					size='$4'
-					backgroundColor={COLORS.tertiary}
-					hoverStyle={{backgroundColor: COLORS.secondary, borderColor: COLORS.secondary}}
-					focusStyle={{backgroundColor: COLORS.secondary}}
-				>
-					Submit
-				</Button>
-			</Form.Trigger>
-		</Form>
+		{({ handleChange, handleBlur, handleSubmit, values }) => (
+		<View
+			flex={1}
+			flexDirection='column'
+			justifyContent='center'
+			alignItems='center'
+			gap={SIZES.large}
+			padding={SIZES.large}
+			margin={'auto'}
+			width='100%'
+		>
+			<Input
+				onChangeText={handleChange('username')}
+				onBlur={handleBlur('username')}
+				value={values.username}
+			/>
+			<Input
+				onChangeText={handleChange('email')}
+				onBlur={handleBlur('email')}
+				value={values.email}
+			/>
+			<Input
+				onChangeText={handleChange('password')}
+				onBlur={handleBlur('password')}
+				value={values.password}
+			/>
+			<Input
+				onChangeText={handleChange('confirmPassword')}
+				onBlur={handleBlur('confirmPassword')}
+				value={values.confirmPassword}
+			/>
+			<Button disabled={status !== 'off'} onPress={handleSubmit} title="Submit">
+				Register
+			</Button>
+		</View>
+		)}
+	</Formik>
 	)
 }
 
